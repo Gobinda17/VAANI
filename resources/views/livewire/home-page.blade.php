3182cc5246a206913a78a1d1e4a815c2e7cede9e
Gobinda17 <gobindadeb14@gmail.com>
2024-01-08 15:56:53 +0530

    First Commit

---
+++ resources/views/livewire/home-page.blade.php
@@ -0,0 +1,7 @@
+<div>
+    {{-- Because she competes with no one, no one can compete with her. --}}
+    <h1>Hello World!!!!</h1>
+    <h1>
+        <a href="/" wire:navigate>Click Me</a>
+    </h1>
+</div>
